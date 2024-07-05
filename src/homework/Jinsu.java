3c55524eb52b18e5fa8dd8afa97f11028f9ee74b
bitcamp <bitcamp@bit>
2024-07-05 18:16:32 +0900

    commit

--- src/homework/Jinsu.java
+++ src/homework/Jinsu.java
@@ -11,17 +11,20 @@ public class Jinsu {
 		//[조건] 2진수 8진수 16진수로 변환시켜주는 메소드 이용하기 - Integer
 		//[실행결과]
 		// 10진수 데이터 입력 : 250
+		// 2진수 = 1111 0000
+		// 8진수 = 372
+		// 16진수 = fa
+
 		Scanner scan = new Scanner(System.in);
 		System.out.print("10진수 데이터 입력 : ");
-		int a = scan.nextInt();
-		// 2진수 = 1111 0000
-		String bin = Integer.toBinaryString(a);
+		int dec = scan.nextInt();
+		
+		String bin = Integer.toBinaryString(dec);
+		String oct = Integer.toOctalString(dec);
+		String hex = Integer.toHexString(dec);
+
 		System.out.println("2진수 : " + bin);
-		// 8진수 = 372
-		String oct = Integer.toOctalString(a);
 		System.out.println("8진수 : " + oct);
-		// 16진수 = fa
-		String hex = Integer.toHexString(a);
 		System.out.println("8진수 : " + hex);
 	}
 
--- src/operator/Salary.java
+++ src/operator/Salary.java
@@ -1,4 +1,4 @@
-package homework;
+package operator;
 
 import java.text.DecimalFormat;
 import java.util.Scanner;
@@ -15,6 +15,10 @@ public class Salary {
 		//월급 = 합계 - 세금
 
 		//[조건]
+		//1. 조건연산자 세율 
+		// 5,000,000이상 3% (0.03)
+		// 3,000,000이상 2% (0.02)
+		// 1,000,000이상 1% (0.01)
 		//2. 숫자는 3자리마다 , 표시
 		//3. 소수이하는 표시하지 않는다. (정수형)  
 
@@ -34,6 +38,7 @@ public class Salary {
 		String name, rank; 
 		int BasicPay, ExtraPay;
 		double tot=0, tax=0, salary=0;
+		double rate=0;
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.print("이름 입력 : ");
@@ -46,8 +51,10 @@ public class Salary {
 		ExtraPay = scan.nextInt();
 		
 		tot = BasicPay + ExtraPay;
-		tax = tot * 0.03;
+		rate = (tot>=5000000?0.03:(tot>=3000000?0.02:0.01));
+		tax = tot * rate;
 		salary = tot - tax;
+	
 		
 		System.out.println("*** "+ name + " " +rank +" 월급 ***");
 		System.out.println("기본급 : " + new DecimalFormat().format(BasicPay) +"원");
@@ -55,6 +62,8 @@ public class Salary {
 		System.out.println("합계 : " + new DecimalFormat().format((int)tot)+"원");
 		System.out.println("세금 : " + new DecimalFormat().format((int)tax)+"원");
 		System.out.println("월급 : " + new DecimalFormat().format((int)salary)+"원");
+
+		System.out.println(rate);
 	}
 
 }
